c72a5f527d7e9c9684a117fadd8dfae13cc06359
Scott Pak <gpak86@gmail.com>
2024-01-27 11:47:41 -0500

    added logo in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import './Header.css';
+{/*import './Header.css';*/}
+import styles from './Header.module.css';
+import onlineshopping from './OnlineShopping.png';
 
 function Header() {
     return (
         <header className="banner-header">
+            <img className={styles.logo} alt="Online Shop" src={onlineshopping} />
             <h1>My Shopping List</h1>
         </header>
     );
